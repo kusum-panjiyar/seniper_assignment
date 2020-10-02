c6b7073fa1d4a2a794e7dc4b85f3cd37f311b2c5
Kusum <phenomenal_ab@linuxmail.org>
2020-10-03 00:22:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route, Link } from 'react-router-dom'
+import UForm from "./Form"
+import DashBoard from "./DashBoard"
+
+function App() {
+  return (
+    <BrowserRouter>
+        <div>
+            <Link to="/">Form</Link> - 
+            <Link to="/DashBoard">Table</Link> 
+
+            <Route path="/" component={UForm} exact={true} />
+            <Route path="/dashboard" component={DashBoard} />
+        </div>
+        </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Form.js
@@ -0,0 +1,153 @@
+import React from "react"
+import {Container, Form, Col, Row, Button} from 'react-bootstrap'
+
+class UForm extends React.Component(){
+        constructor(props){
+            super(props)
+            this.state = {
+                formName: 'Aromatic bar',
+                text: '',
+                email: '',
+                phone: '',
+                name: '',
+                rating: '',
+                arr:[]
+            }
+        }
+    
+     handleSubmit = (e) => {
+        e.preventDefault()
+        const formData = {
+            formName: this.state.formName,
+            text: this.state.text,
+            email: this.state.email,
+            phone: this.state.phone,
+            name: this.state.name,
+            gender: this.state.gender,
+            
+        }
+
+        //const arr=[]
+        console.log(formData)
+         JSON.parse(localStorage.getItem('arr'))
+         arr.push(formData)
+        
+        localStorage.setItem('arr',JSON.stringify(arr))
+    }
+
+     handleChange = (e) => {
+        this.setState({
+            [e.target.name]:e.target.value 
+        })
+    }
+
+     handleRadioChange=(gender)=>{
+        this.setState({gender})
+    }
+
+    render(){
+        return (
+            <div>
+                <Container>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Row>
+                            <Col>
+                            <input className="ml-0 border-0 text-left text-info mt-3" style={{outline: "none"}}
+                                    value={this.state.formName}
+                                />
+                                <p>we are committed to providing you with the best dining experience possible,
+                                    so we welcome your comments. please fill out these questionnarie Thank you
+                                </p>
+                            </Col>
+                        </Row>
+                        <Row className="pt-3">
+                            <Col md={6}>
+                                <Form.Label htmlFor="name">Text Field</Form.Label>
+                                <Form.Control 
+                                    type="text"
+                                    id="text"
+                                    name="text"
+                                    value={this.state.text}
+                                    onChange={this.handleChange}
+                                    required
+                                />
+                            </Col>
+                            <Col md={6}>
+                            <Form.Label htmlFor="email">Email:-</Form.Label>
+                            <Form.Control 
+                                type="text"
+                                id="email"
+                                name="email"
+                                value={this.state.email}
+                                onChange={this.handleChange}
+                                required
+                            />
+                            </Col>
+                        </Row>
+    
+                       
+                        <Row className="pt-3">
+                            <Col md={6}>
+                                <Form.Label htmlFor="phone">Phone:-</Form.Label>
+                                <Form.Control 
+                                    type="Number"
+                                    id="phone"
+                                    name="phone"
+                                    value={this.state.phone}
+                                    onChange={this.handleChange}
+                                    required
+                                    pattern="[0-9]{10}"
+                                />
+                            </Col>
+                            <Col md={6}>
+                            <Form.Label htmlFor="name">Name:-</Form.Label>
+                            <Form.Control 
+                                type="text"
+                                id="name"
+                                name="name"
+                                value={this.state.name}
+                                onChange={this.handleChange}
+                                required
+                            />
+                            </Col>
+                        </Row>
+
+                         <Row>
+                            <Col>
+                            <Form.Label>Rating:- </Form.Label>
+                                <Form.Check inline label='Excellent' type="radio" id="excellent" name="rating"
+                                    checked={this.state.rating ==='excellent'}
+                                    onChange={()=>{this.handleRadioChange('excellent')}}
+                                />
+                                <Form.Check inline label='Good' type="radio" id="good" name="rating"
+                                    checked={this.state.rating==='good'}
+                                    onChange={()=>{this.handleRadioChange('good')}}
+                                />
+                                <Form.Check inline label='Fair' type="radio" id="fair" name="rating"
+                                    checked={this.state.rating==='fair'}
+                                    onChange={()=>{this.handleRadioChange('fair')}}
+                                />
+                                <Form.Check inline label='Bad' type="radio" id="bad" name="rating"
+                                    checked={this.state.rating==='bad'}
+                                    onChange={()=>{this.handleRadioChange('bad')}}
+                                />
+                            </Col>                       
+                        </Row>
+    
+                        <Row>
+                            <Col md={4}></Col>
+                            <Col md={4}>
+                        <Button  size="sm" block  className="mt-3 pl-5 pr-5">
+                            <input type="submit" className="btn" value="SUBMIT"/></Button>
+                            </Col>
+                            <col md={4}></col>
+                        </Row>
+                    </Form>
+                </Container>
+            </div>
+        )
+    }
+    
+}
+
+export default UForm
